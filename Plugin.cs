7491de1269df04b7783533ffed14d403263ad5b2
basmannetjee <156799780+basmannetjee@users.noreply.github.com>
2024-01-23 20:32:44 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WhistlingGhost
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static AudioClip Audio;
+        private void Awake()
+        {
+            string location = Info.Location.TrimEnd($"{PluginInfo.PLUGIN_GUID}.dll".ToCharArray());
+            UnityWebRequest audioLoader = UnityWebRequestMultimedia.GetAudioClip($"File://{location}deathwhistle.mp3", AudioType.MPEG);
+
+            audioLoader.SendWebRequest();
+            while (!audioLoader.isDone) { }
+
+            if (audioLoader.result == UnityWebRequest.Result.Success)
+            {
+                Audio = DownloadHandlerAudioClip.GetContent(audioLoader);
+
+                new Harmony(PluginInfo.PLUGIN_GUID).PatchAll(typeof(GirlPatch));
+                Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            }
+            else
+            {
+                Logger.LogError($"Could not load audio file");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DressGirlAI))]
+    internal class GirlPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void AudioPatch(DressGirlAI __instance)
+        {
+            __instance.breathingSFX = Plugin.Audio;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Death whistle ghost girl
+
+
+Changes the breathing SFX to the death whistle.
